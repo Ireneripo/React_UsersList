8657b73fee8fc12d9cc488cc93bb3c15bf4944bc
Irene Riportella <riportellairene@gmail.com>
2022-09-16 10:24:57 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import AddUser from './components/Users/AddUser';
+// import Card from './components/UI/Card';
+// import styles from './components/UI/Card.module.css';
 
 function App() {
+  const userName = props.userName;
+  const age = props.age;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Card className={'card'}>
+        <div>{userName}</div>
+        <div>{age}</div>
+      </Card>
     </div>
   );
 }
---
+++ src/components/UI/Card.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import AddUser from '../Users/AddUser';
+
+const Card = (props) => {
+  // return (
+  //     <AddUser></AddUser>
+  // );
+};
+
+export default Card;
